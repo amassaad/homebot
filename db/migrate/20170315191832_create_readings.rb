6ff05918502deee4a1f0c36bf28179a0af7d0f78
Alex Massaad <alex.massaad@shopify.com>
2017-03-15 15:19:55 -0400

    Add and migrate readings table

---
+++ db/migrate/20170315191832_create_readings.rb
@@ -0,0 +1,11 @@
+class CreateReadings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :readings do |t|
+      t.datetime :time
+      t.integer :amount
+      t.integer :cost
+
+      t.timestamps
+    end
+  end
+end
