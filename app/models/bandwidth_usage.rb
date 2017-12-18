de37bd5c2bda50d1f7b5452c71d502b0b8b4896c
Alex Massaad <alex.massaad@shopify.com>
2017-12-18 18:07:23 -0500

    Refactor BandwidthUsage to remove retries

--- app/models/bandwidth_usage.rb
+++ app/models/bandwidth_usage.rb
@@ -1,4 +1,6 @@
 class BandwidthUsage < ApplicationRecord
+  require 'httparty'
+
   validates :on_peak_download, :on_peak_upload, :off_peak_download, :off_peak_upload, presence: true
   validates :period, uniqueness: true
 
@@ -11,23 +13,24 @@ class BandwidthUsage < ApplicationRecord
   end
 
   def self.parse_usage_summary(slice)
-    require 'httparty'
     ENV['TEK_API'] || raise('no TEK_API provided')
-    tries ||= 10
     access_header = {"TekSavvy-APIKey" => ENV['TEK_API']}
-    #detail
-    # response = HTTParty.get('https://api.teksavvy.com/web/Usage/UsageRecords', headers: access_header)
-    response = HTTParty.get('https://api.teksavvy.com/web/Usage/UsageSummaryRecords', headers: access_header)
-    date = DateTime.strptime(response['value'][slice]['StartDate'].to_s, "%Y-%m-%d")
-    @bandwidth = BandwidthUsage.where(period: date).first_or_create
-    @bandwidth.on_peak_download = response['value'][slice]['OnPeakDownload']
-    @bandwidth.off_peak_download = response['value'][slice]['OffPeakDownload']
-    @bandwidth.on_peak_upload = response['value'][slice]['OnPeakUpload']
-    @bandwidth.off_peak_upload = response['value'][slice]['OffPeakUpload']
-    @bandwidth.save!
-  rescue NoMethodError
-    retry unless (tries -= 1).zero?
-  else
-    logger.info("Successfully fetched bandwidth")
+    begin
+      # tries ||= 10
+      #detail
+      # response = HTTParty.get('https://api.teksavvy.com/web/Usage/UsageRecords', headers: access_header)
+      response = HTTParty.get('https://api.teksavvy.com/web/Usage/UsageSummaryRecords', headers: access_header)
+      date = DateTime.strptime(response['value'][slice]['StartDate'].to_s, "%Y-%m-%d")
+      @bandwidth = BandwidthUsage.where(period: date).first_or_create
+      @bandwidth.on_peak_download = response['value'][slice]['OnPeakDownload']
+      @bandwidth.off_peak_download = response['value'][slice]['OffPeakDownload']
+      @bandwidth.on_peak_upload = response['value'][slice]['OnPeakUpload']
+      @bandwidth.off_peak_upload = response['value'][slice]['OffPeakUpload']
+      @bandwidth.save!
+      logger.info("Successfully fetched bandwidth")
+    rescue NoMethodError
+      logger.error("Error, retrying")
+      # retry unless (tries -= 1).zero?
+    end
   end
 end
